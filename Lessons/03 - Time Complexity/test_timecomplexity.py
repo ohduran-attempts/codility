33c8f0b0824bd5a41f18e91216e5e1175e792325
ohduran <alvaro.duranb@gmail.com>
2018-06-06 19:03:12 +0100

    Chapter 3 challenge tests setup

---
+++ Lessons/03 - Time Complexity/test_timecomplexity.py
@@ -0,0 +1,26 @@
+import unittest
+import frgjmp.py
+import permMissingElem
+import tapeEquilibrium
+
+
+class TestFRGJMP(unittest.TestCase):
+    """Test FRGJMP Challenge."""
+
+    pass
+
+
+class TestPermMissingElem(unittest.TestCase):
+    """Test permMissingElem Challenge."""
+
+    pass
+
+
+class TestTapeEquilibrium(unittest.TestCase):
+    """Test tapeEquilibrium Challenge."""
+
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
