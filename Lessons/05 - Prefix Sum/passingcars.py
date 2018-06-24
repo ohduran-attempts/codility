ad5bae30d05443a33a18f56db88f121eca637883
ohduran <alvaro.duranb@gmail.com>
2018-06-24 14:41:21 +0100

    Test passed but performance is not O(n2)

--- Lessons/05 - Prefix Sum/passingcars.py
+++ Lessons/05 - Prefix Sum/passingcars.py
@@ -37,13 +37,19 @@ expected worst-case space complexity is O(1) (not counting the storage required 
 
 
 def passingcars(A):
+    """
+    We say that a pair of cars (P, Q),
+    P less than Q then less than N, and all bigger than 0, is passing
+    when P is traveling to the east and Q is traveling to the west.
+    """
     EAST = 0
     WEST = 1
 
     paired_cars = 0
 
-    for index, car in enumerate(A):
-        for second_car in A[index:]:
-            if second_car != car:
+    for index in range(len(A) - 1):
+        car = A[index]
+        for second_car in A[index + 1:]:
+            if car == EAST and second_car == WEST:
                 paired_cars += 1
     return paired_cars
